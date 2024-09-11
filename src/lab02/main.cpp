447ee4bc161cf7dc4ecf45ed1e7d9c359541228f
SERGEYDJUM <39106181+SERGEYDJUM@users.noreply.github.com>
2024-09-12 00:25:12 +0300

    Add Surface

--- src/lab02/main.cpp
+++ src/lab02/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <optional>
 #include <stdexcept>
 #include <unordered_set>
 
@@ -11,11 +12,21 @@
 
 class App {
    public:
-    App() : vk_instance{initInstance()} {
-        glfwInit();
-        checkVkLayers();
-        checkVkExtensions();
+    /// @brief Initializes GLFW and Vulkan components in the correct order
+    void init() {
+        initGlfw();
+        gatherVkLayers();
+        gatherVkExtensions();
+        initInstance();
         initWindow();
+        initSurface();
+    }
+
+    /// @brief Runs window's event loop
+    void runLoop() {
+        while (!glfwWindowShouldClose(window)) {
+            glfwPollEvents();
+        }
     }
 
     ~App() {
@@ -23,41 +34,59 @@ class App {
         glfwTerminate();
     }
 
-    void run() { eventLoop(); }
-
    private:
-    const std::pair<uint32_t, uint32_t> window_dims{800, 600};
+    const vk::Extent2D surface_extent{800, 600};
+    const vk::raii::Context vk_context{};
 
-    vk::raii::Context vk_context;
-    vk::raii::Instance vk_instance;
+#ifndef NDEBUG
+    const bool enable_validation_layers = true;
+#else
+    const bool enable_validation_layers = false;
+#endif
+
+    std::optional<vk::raii::Instance> vk_instance;
+    std::optional<vk::raii::SurfaceKHR> vk_surface;
+    std::vector<const char*> instance_extensions;
+    std::vector<const char*> instance_layers;
 
     GLFWwindow* window = nullptr;
 
-#ifdef NDEBUG
-    const bool enable_validation_layers = false;
-#else
-    const bool enable_validation_layers = true;
-#endif
+    /// @brief Initializes GLFW and sets error callback
+    void initGlfw() {
+        glfwInit();
+        glfwSetErrorCallback([](int error, const char* msg) {
+            std::cerr << "glfw: "
+                      << "(" << error << ") " << msg << std::endl;
+        });
+    }
 
-    void checkVkLayers() {
+    /// @brief Checks availability of necessary layers and records them
+    void gatherVkLayers() {
+        const char* validation_layer_name = "VK_LAYER_KHRONOS_validation";
         bool validation_layer_present = false;
 
         for (auto layer : vk_context.enumerateInstanceLayerProperties()) {
+            // Couldn't avoid double cast
             std::string layername{static_cast<const char*>(layer.layerName)};
 #ifndef NDEBUG
             std::cout << layername << std::endl;
 #endif
-            if (layername == "VK_LAYER_KHRONOS_validation") {
+            if (layername == validation_layer_name) {
                 validation_layer_present = true;
             }
         }
 
-        if (enable_validation_layers && !validation_layer_present) {
-            throw std::runtime_error("vulkan validation layer missing");
+        if (enable_validation_layers) {
+            if (!validation_layer_present) {
+                throw std::runtime_error("vulkan validation layer missing");
+            }
+
+            instance_layers.emplace_back(validation_layer_name);
         }
     }
 
-    void checkVkExtensions() {
+    /// @brief Checks availability of necessary extensions and records them
+    void gatherVkExtensions() {
         uint32_t glfw_extensions_cnt = 0;
         const char** glfw_extensions =
             glfwGetRequiredInstanceExtensions(&glfw_extensions_cnt);
@@ -81,42 +110,59 @@ class App {
                 throw std::runtime_error(
                     "vulkan extension(s) required by GLFW missing");
             }
+
+            instance_extensions.emplace_back(glfw_extensions[i]);
         }
 
-        if (enable_validation_layers &&
-            !extension_set.contains(VK_EXT_DEBUG_UTILS_EXTENSION_NAME)) {
-            throw std::runtime_error("vulkan debug utils extension missing");
+        if (enable_validation_layers) {
+            if (!extension_set.contains(VK_EXT_DEBUG_UTILS_EXTENSION_NAME)) {
+                throw std::runtime_error(
+                    "vulkan debug utils extension missing");
+            }
+            instance_extensions.emplace_back(VK_EXT_DEBUG_UTILS_EXTENSION_NAME);
         }
     }
 
-    vk::raii::Instance initInstance() {
+    /// @brief Creates a Vulkan instance with recorded layers and extensions
+    void initInstance() {
         vk::ApplicationInfo app_info{"App", 1, "Engine", 1, VK_API_VERSION_1_3};
+        vk::InstanceCreateInfo inst_info(vk::InstanceCreateFlags{}, &app_info);
 
-        vk::InstanceCreateInfo instance_info(vk::InstanceCreateFlags(),
-                                             &app_info);
+        inst_info.enabledExtensionCount = instance_extensions.size();
+        inst_info.ppEnabledExtensionNames = instance_extensions.data();
+        inst_info.enabledLayerCount = instance_layers.size();
+        inst_info.ppEnabledLayerNames = instance_layers.data();
 
-        return vk_context.createInstance(instance_info);
+        vk_instance = vk_context.createInstance(inst_info);
     }
 
+    /// @brief Creates an empty window
     void initWindow() {
         glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
         glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
-
-        window = glfwCreateWindow(window_dims.first, window_dims.second,
-                                  "Vulkan", nullptr, nullptr);
+        window = glfwCreateWindow(surface_extent.width, surface_extent.height,
+                                  "App", nullptr, nullptr);
     }
 
-    void eventLoop() {
-        while (!glfwWindowShouldClose(window)) {
-            glfwPollEvents();
+    /// @brief Creates a Vulkan surface on the window
+    void initSurface() {
+        VkSurfaceKHR surface;
+
+        if (glfwCreateWindowSurface(*vk_instance.value(), window, nullptr,
+                                    &surface) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create window surface");
         }
+
+        vk_surface = {vk_instance.value(), surface};
     }
 };
 
 int main() {
+    App app;
+
     try {
-        App app{};
-        app.run();
+        app.init();
+        app.runLoop();
     } catch (vk::SystemError& e) {
         std::cerr << e.what() << std::endl;
         return EXIT_FAILURE;
